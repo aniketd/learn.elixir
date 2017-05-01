939d0f7a8502331168ead29d0088970c4a2c2384
Aniket Deshpande <anicake@gmail.com>
2017-05-01 19:39:18 +0530

    More progress.

--- mix-otp-app/kv/test/kv/registry_test.exs
+++ mix-otp-app/kv/test/kv/registry_test.exs
@@ -2,8 +2,8 @@ defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
   setup context do
-    {:ok, registry} = KV.Registry.start_link(context.test)
-    {:ok, registry: registry}
+    {:ok, _} = KV.Registry.start_link(context.test)
+    {:ok, registry: context.test}
   end
 
   test "spawns buckets", %{registry: registry} do
@@ -20,20 +20,24 @@ defmodule KV.RegistryTest do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
+    # Ensure :DOWN was processed by registry
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
   test "removes bucket on crash", %{registry: registry} do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    ref = Process.monitor(bucket)
 
     # Stop the bucket with non-normal reason
-    ref = Process.monitor(bucket)
     Process.exit(bucket, :shutdown)
 
     # Wait until the bucket is dead
     assert_receive {:DOWN, ^ref, _, _, _}
 
+    # Ensure :DOWN was processed by registry
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 end
